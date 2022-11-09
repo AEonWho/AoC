e787a12318e8e903775fadbc5c906aaebe97e5dd
AEonWho <thomas.beer@outlook.com>
2022-11-09 15:12:50 +0100

    2015 - Day 02 added

---
+++ AoC_2015_02/Package.cs
@@ -0,0 +1,29 @@
+﻿internal class Package
+{
+    public Package(string input)
+    {
+        var data = input.Split('x').Select(int.Parse).ToArray();
+
+        LengthA = data[0];
+        LengthB = data[1];
+        LengthC = data[2];
+
+        SideA = LengthA * LengthB;
+        SideB = LengthA * LengthC;
+        SideC = LengthB * LengthC;
+
+        var ribbon = new[] { LengthA, LengthB, LengthC }.OrderBy(c => c).Take(2).Sum() * 2;
+        Ribbon = ribbon + (LengthA * LengthB * LengthC);
+
+        WrappingPaperNeeded = (SideA + SideB + SideC) * 2 + new[] { SideA, SideB, SideC }.Min();
+    }
+
+    public int LengthA { get; }
+    public int LengthB { get; }
+    public int LengthC { get; }
+    public int SideA { get; }
+    public int SideB { get; }
+    public int SideC { get; }
+    public int Ribbon { get; }
+    public int WrappingPaperNeeded { get; }
+}
---
+++ AoC_2015_02/Program.cs
@@ -0,0 +1,8 @@
+﻿var lines = File.ReadAllLines("Input.txt");
+
+var packages = lines.Select(c => new Package(c));
+
+var wrap = packages.Sum(d => d.WrappingPaperNeeded);
+var ribbon = packages.Sum(c => c.Ribbon);
+Console.WriteLine("WrappingPaper: "+ wrap);
+Console.WriteLine("Ribbon: "+ ribbon);
