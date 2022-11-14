57135601d5b348283ba9ff3c95fe16441eac7a1c
AEonWho <thomas.beer@outlook.com>
2022-11-15 00:19:04 +0100

    2015 - Day 08 added

---
+++ AoC_2015_08/Program.cs
@@ -0,0 +1,62 @@
+﻿var input = File.ReadAllLines("Input.txt");
+//var input = File.ReadAllLines("Sample.txt");
+
+int size = 0;
+foreach (var line in input)
+{
+    var length = line.Length;
+
+    var contentLength = 0;
+    for (int i = 1; i < length - 1; i++)
+    {
+        contentLength++;
+        switch (line[i])
+        {
+            case '\\':
+                if (line[i + 1] == 'x')
+                {
+                    i += 3;
+                }
+                else
+                {
+                    i += 1;
+                }
+                break;
+        }
+    }
+
+    size += length - contentLength;
+}
+
+Console.WriteLine(size);
+
+
+size = 0;
+foreach (var line in input)
+{
+    var length = line.Length;
+
+    var addedLength = 4;
+    for (int i = 1; i < length - 1; i++)
+    {
+        switch (line[i])
+        {
+            case '\\':
+                if (line[i + 1] == 'x')
+                {
+                    addedLength++;
+                    i += 3;
+                }
+                else
+                {
+                    addedLength += 2;
+                    i += 1;
+                }
+                break;
+        }
+    }
+
+    size += addedLength;
+}
+
+Console.WriteLine(size);
