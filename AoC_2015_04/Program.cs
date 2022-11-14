9f2fb165aa11adbb8c712aace49a7963545a3c41
AEonWho <thomas.beer@outlook.com>
2022-11-14 22:04:55 +0100

    2015 - Day 04 added

---
+++ AoC_2015_04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+var input = "yzbqklnj";
+
+var algo = MD5.Create();
+
+int i = 1;
+while (true)
+{
+    var resByte = algo.ComputeHash(Encoding.ASCII.GetBytes($"{input}{i}"));
+    var resStr = BitConverter.ToString(resByte);
+
+    if (resStr.StartsWith("00-00-0"))
+    {
+        Console.WriteLine($"Input: {input}{i} results in 5 leading zeros");
+    }
+
+    if (resStr.StartsWith("00-00-00"))
+    {
+        Console.WriteLine($"Input: {input}{i} results in 6 leading zeros");
+        break;
+    }
+
+    i++;
+}
+
+Console.ReadLine();
