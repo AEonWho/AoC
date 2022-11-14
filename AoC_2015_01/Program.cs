d3290b41ba70af9eb6fbc6edd26ff0ea78f79434
AEonWho <thomas.beer@outlook.com>
2022-11-14 22:12:09 +0100

    2015 - Day 01 added

---
+++ AoC_2015_01/Program.cs
@@ -0,0 +1,25 @@
+﻿var txt = File.ReadAllText("Input.txt");
+
+var x = 0;
+
+for (int idx = 0; idx < txt.Length; idx++)
+{
+    var c = txt[idx];
+
+    switch (c)
+    {
+        case '(':
+            x++;
+            break;
+        case ')':
+            x--;
+
+            if (x == -1)
+            {
+                Console.WriteLine($"Santa is in Basement at idx {idx+1}");
+            }
+            break;
+    }
+}
+
+Console.WriteLine($"Santa ends on Floor {x}");
