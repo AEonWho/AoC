08b614679ea545890422dd759bae14e91b2df964
AEonWho <thomas.beer@outlook.com>
2022-11-15 20:49:58 +0100

    2021 - Day 01 added

---
+++ AoC_2021_01/Program.cs
@@ -0,0 +1,24 @@
+﻿var lines = File.ReadAllLines("Input.txt").Select(int.Parse).ToList();
+
+int increments = 0;
+
+for (int i = 1; i < lines.Count; i++)
+{
+    if (lines[i - 1] < lines[i])
+        increments++;
+}
+
+Console.WriteLine($"Increments: {increments}");
+
+increments = 0;
+
+for (int i = 3; i < lines.Count; i++)
+{
+    var val1 = lines[i - 3] + lines[i - 2] + lines[i - 1];
+    var val2 = lines[i - 2] + lines[i - 1] + lines[i];
+
+    if (val1< val2)
+        increments++;
+}
+
+Console.WriteLine($"Increments: {increments}");
