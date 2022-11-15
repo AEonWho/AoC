80494aafc63b5f65974929a03371c3bd48628b7e
AEonWho <thomas.beer@outlook.com>
2022-11-15 20:23:18 +0100

    2015 - Day 10 added

---
+++ AoC_2015_10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+var input = "1113122113";
+
+for (int i = 0; i < 50; i++)
+{
+    input = Iterate(input);
+
+
+    Console.WriteLine($"Iteration{i + 1}: {input.Length}");
+}
+
+
+string Iterate(string input)
+{
+    StringBuilder sb = new StringBuilder();
+
+    var checkChar = input[0];
+    var currentCount = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (checkChar == input[i])
+        {
+            currentCount++;
+        }
+        else
+        {
+            sb.Append($"{currentCount}{checkChar}");
+            checkChar = input[i];
+            currentCount = 1;
+        }
+
+    }
+    sb.Append($"{currentCount}{checkChar}");
+
+    return sb.ToString();
+}
