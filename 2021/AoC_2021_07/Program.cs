ad2d4e4e55629a3b7cd76625557f100210efd7cd
AEonWho <thomas.beer@outlook.com>
2022-11-21 19:54:01 +0100

    2021 - Day 07 added

---
+++ 2021/AoC_2021_07/Program.cs
@@ -0,0 +1,23 @@
+﻿var values = File.ReadAllText("Input.txt").Split(',').Select(int.Parse).OrderBy(c => c).ToList();
+
+var lowest = values.Min();
+var highest = values.Max();
+
+Dictionary<int, int> sumStage1 = new Dictionary<int, int>();
+Dictionary<int, int> sumStage2 = new Dictionary<int, int>();
+for (int i = lowest; i <= highest; i++)
+{
+    sumStage1.Add(i, values.Sum(d => Math.Abs(i - d)));
+    sumStage2.Add(i, values.Sum(d => SumOverN(Math.Abs(i - d))));
+}
+
+var lowestvalue1 = sumStage1.Min(c => c.Value);
+Console.WriteLine(lowestvalue1);
+
+var lowestvalue2 = sumStage2.Min(c => c.Value);
+Console.WriteLine(lowestvalue2);
+
+int SumOverN(int val)
+{
+    return (val * (val + 1)) / 2;
+}
