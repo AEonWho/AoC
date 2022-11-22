0a796dde1c2b21e36553e43a5411b37161ebd20b
AEonWho <thomas.beer@outlook.com>
2022-11-22 23:05:13 +0100

    2021 - Day 22 Part1 added

---
+++ 2021/AoC_2021_22/Program.cs
@@ -0,0 +1,35 @@
+﻿var lines = File.ReadAllLines("Input.txt").Take(20);
+
+List<HashSet<(int x, int y, int z)>> onCubes = new List<HashSet<(int x, int y, int z)>>();
+
+foreach (var line in lines)
+{
+    var data = line.Split(new string[] { " x=", ",y=", ",z=", ".." }, StringSplitOptions.RemoveEmptyEntries);
+
+    var coords = data.Skip(1).Select(int.Parse).ToArray();
+
+    HashSet<(int, int, int)> cubeList = new HashSet<(int, int, int)>();
+    for (int x = coords[0]; x <= coords[1]; x++)
+    {
+        for (int y = coords[2]; y <= coords[3]; y++)
+        {
+            for (int z = coords[4]; z <= coords[5]; z++)
+            {
+                if (data[0] == "on")
+                {
+                    cubeList.Add((x, y, z));
+                }
+                else
+                {
+                    onCubes.ForEach(d => d.Remove((x, y, z)));
+                }
+            }
+        }
+    }
+    if (cubeList.Any())
+    {
+        onCubes.Add(cubeList);
+    }
+}
+
+Console.WriteLine(onCubes.SelectMany(c=>c).ToHashSet().Count());
