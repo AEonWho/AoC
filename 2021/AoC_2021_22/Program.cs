35a4b550a793a86bb24771f95f8886a50112dee6
AEonWho <thomas.beer@outlook.com>
2022-11-25 18:55:40 +0100

    2021 - Day 22 Part2 completed

--- 2021/AoC_2021_22/Program.cs
+++ 2021/AoC_2021_22/Program.cs
@@ -1,35 +1,105 @@
-﻿var lines = File.ReadAllLines("Input.txt").Take(20);
+﻿var lines = File.ReadAllLines("Input.txt");
 
-List<HashSet<(int x, int y, int z)>> onCubes = new List<HashSet<(int x, int y, int z)>>();
+var Cubes = new List<Cube>();
 
 foreach (var line in lines)
 {
     var data = line.Split(new string[] { " x=", ",y=", ",z=", ".." }, StringSplitOptions.RemoveEmptyEntries);
 
-    var coords = data.Skip(1).Select(int.Parse).ToArray();
+    var num = data.Skip(1).Select(int.Parse).ToArray();
 
-    HashSet<(int, int, int)> cubeList = new HashSet<(int, int, int)>();
-    for (int x = coords[0]; x <= coords[1]; x++)
+    Cubes.Add(new Cube(num[0], num[1], num[2], num[3], num[4], num[5], data[0] == "on"));
+}
+
+var xValuesEnds = Cubes.SelectMany(c => new[] { c.X1 - 1, c.X2 }).Distinct().OrderBy(c => c).ToArray();
+var yValuesEnds = Cubes.SelectMany(c => new[] { c.Y1 - 1, c.Y2 }).Distinct().OrderBy(c => c).ToArray();
+var zValuesEnds = Cubes.SelectMany(c => new[] { c.Z1 - 1, c.Z2 }).Distinct().OrderBy(c => c).ToArray();
+
+HashSet<((int, int) X, (int, int) Y, (int, int) Z)> areas = new HashSet<((int, int), (int, int), (int, int))>();
+
+int i = 0;
+foreach (var cube in Cubes)
+{
+    i++;
+    List<(int, int)> xValues = new List<(int, int)>();
+    List<(int, int)> yValues = new List<(int, int)>();
+    List<(int, int)> zValues = new List<(int, int)>();
+
+    splitCube(xValuesEnds, cube.X1, cube.X2, xValues);
+    splitCube(yValuesEnds, cube.Y1, cube.Y2, yValues);
+    splitCube(zValuesEnds, cube.Z1, cube.Z2, zValues);
+
+    foreach (var xEntry in xValues)
     {
-        for (int y = coords[2]; y <= coords[3]; y++)
+        foreach (var yEntry in yValues)
         {
-            for (int z = coords[4]; z <= coords[5]; z++)
+            foreach (var zEntry in zValues)
             {
-                if (data[0] == "on")
+                if (cube.State)
                 {
-                    cubeList.Add((x, y, z));
+                    areas.Add((xEntry, yEntry, zEntry));
                 }
                 else
                 {
-                    onCubes.ForEach(d => d.Remove((x, y, z)));
+                    areas.Remove((xEntry, yEntry, zEntry));
                 }
             }
         }
     }
-    if (cubeList.Any())
+
+    if(i==20)
     {
-        onCubes.Add(cubeList);
+        var volumeStage1 = areas.Sum(GetVolume);
+
+        Console.WriteLine("Stage1: "+volumeStage1);
     }
 }
 
-Console.WriteLine(onCubes.SelectMany(c=>c).ToHashSet().Count());
+var volumeStage2 = areas.Sum(GetVolume);
+
+Console.WriteLine("Stage2: " + volumeStage2);
+
+long GetVolume(((int, int) X, (int, int) Y, (int, int) Z) arg)
+{
+    var bla = Math.Abs((long)arg.X.Item2 - arg.X.Item1) + 1;
+    var bla1 = Math.Abs((long)arg.Y.Item2 - arg.Y.Item1) + 1;
+    var bla2 = Math.Abs((long)arg.Z.Item2 - arg.Z.Item1) + 1;
+
+    return bla * bla1 * bla2;
+}
+
+Console.WriteLine();
+
+void splitCube(int[] xValuesEnds, int x1, int x2, List<(int, int)> xValues)
+{
+    var startX = x1;
+    while (startX <= x2)
+    {
+        var endX = xValuesEnds.First(d => d >= startX);
+        xValues.Add((startX, endX));
+        startX = endX + 1;
+    }
+}
+
+class Cube
+{
+    public Cube(int x1, int x2, int y1, int y2, int z1, int z2, bool state)
+    {
+        X1 = x1;
+        X2 = x2;
+        Y1 = y1;
+        Y2 = y2;
+        Z1 = z1;
+        Z2 = z2;
+        State = state;
+    }
+
+    public bool State { get; private set; }
+
+    public int X1 { get; private set; }
+    public int X2 { get; private set; }
+    public int Y1 { get; private set; }
+    public int Y2 { get; private set; }
+    public int Z1 { get; private set; }
+    public int Z2 { get; private set; }
+}
