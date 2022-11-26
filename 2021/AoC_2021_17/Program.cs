001a3351710dda25259a1b3bf2d82a878dc8cfc8
AEonWho <thomas.beer@outlook.com>
2022-11-26 16:28:38 +0100

    2021 - Day 17 Part1 added

---
+++ 2021/AoC_2021_17/Program.cs
@@ -0,0 +1,7 @@
+﻿//var targetX = (20, 30);
+//var targetY = (-10, -5);
+
+var targetX = (79, 137);
+var targetY = (-176, -117);
+
+Console.WriteLine("Stage1:" + Enumerable.Range(1, Math.Abs(targetY.Item1) - 1).Sum());
