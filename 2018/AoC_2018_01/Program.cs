de480b7b894c1fa10a583b3ac8ca2ec9fb999ddc
AEonWho <thomas.beer@outlook.com>
2022-11-29 14:53:57 +0100

    2018 - Day 01 added

---
+++ 2018/AoC_2018_01/Program.cs
@@ -0,0 +1,20 @@
+﻿var values = File.ReadAllLines("Input.txt").Select(int.Parse).ToList();
+
+Console.WriteLine("Stage1: " + values.Sum());
+
+HashSet<long> Frequencies = new HashSet<long>();
+
+var value = 0;
+int i = 0;
+while (!Frequencies.Contains(value))
+{
+    Frequencies.Add(value);
+
+    value += values[i];
+
+    i++;
+    if (i >= values.Count)
+        i = 0;
+}
+
+Console.WriteLine("Stage1: " + value);
