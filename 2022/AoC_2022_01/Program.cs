dc373c3f3320cc141abe2e0aa21216dfb21aec0a
AEonWho <thomas.beer@outlook.com>
2022-12-01 06:08:54 +0100

    2022 - Day 01 added

---
+++ 2022/AoC_2022_01/Program.cs
@@ -0,0 +1,21 @@
+﻿var lines = File.ReadAllLines("Input.txt").ToList();
+
+
+List<List<int>> elves = new List<List<int>>();
+
+
+elves.Add(new List<int>());
+foreach (var line in lines)
+{
+    if (int.TryParse(line, out var val))
+    {
+        elves[^1].Add(val);
+    }
+    else
+    {
+        elves.Add(new List<int>());
+    }
+}
+Console.WriteLine("Stage1: " + elves.Select(c => c.Sum()).Max());
+
+Console.WriteLine("Stage2: " + elves.Select(c => c.Sum()).OrderByDescending(d => d).Take(3).Sum());
