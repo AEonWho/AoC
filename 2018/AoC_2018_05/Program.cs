74e28e26f7c0c8d707881aef562670977b12b804
AEonWho <thomas.beer@outlook.com>
2022-12-03 11:35:52 +0100

    2018 - Day 05 added

---
+++ 2018/AoC_2018_05/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+var chars = File.ReadAllText("Input.txt").ToList();
+
+var result = React(chars.ToList());
+
+Console.WriteLine("Stage1: " + result.Count);
+
+var letters = chars.Where(Char.IsUpper).Distinct().ToList();
+
+int shortest = chars.Count;
+foreach (var letter in letters)
+{
+    var tmp = chars.Where(d => d != letter && d != letter + 32).ToList();
+    var result2 = React(tmp);
+    if (result2.Count < shortest)
+    {
+        shortest = result2.Count;
+    }
+}
+
+Console.WriteLine("Stage2: " + shortest);
+
+List<char> React(List<char> chars)
+{
+    var idx = 0;
+    while (true)
+    {
+        if (idx >= chars.Count - 1)
+            break;
+
+        if (chars[idx] != chars[idx + 1] && (chars[idx] + 32 == chars[idx + 1] || chars[idx] == chars[idx + 1] + 32))
+        {
+            chars.RemoveAt(idx);
+            chars.RemoveAt(idx);
+
+            if (idx > 0)
+                idx--;
+        }
+        else
+        {
+            idx++;
+        }
+    }
+
+    return chars;
+}
