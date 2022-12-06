a8a574b7cf6d0711ad6a4634aefe283041948e87
AEonWho <thomas.beer@outlook.com>
2022-12-06 07:24:51 +0100

    2022 - Day 06 added

---
+++ 2022/AoC_2022_06/Program.cs
@@ -0,0 +1,29 @@
+﻿var line = File.ReadAllText("Input.txt");
+
+for (int i = 4; i < line.Length; i++)
+{
+    var start = i - 4;
+    var end = i;
+
+    var cnt = line[start..end].Distinct().Count();
+
+    if (cnt == 4)
+    {
+        Console.WriteLine("Stage1: " + i);
+        break;
+    }
+}
+
+for (int i = 14; i < line.Length; i++)
+{
+    var start = i - 14;
+    var end = i;
+
+    var cnt = line[start..end].Distinct().Count();
+
+    if (cnt == 14)
+    {
+        Console.WriteLine("Stage2: " + i);
+        break;
+    }
+}
