58bf38b054063a5103a0de1e1bba7a9c47550276
AEonWho <thomas.beer@outlook.com>
2023-12-02 09:36:08 +0100

    2023 - Day 02 added

---
+++ 2023/AoC_2023_02/Game.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Game
+{
+    public int Id { get; }
+
+    public List<(string Color, int Count)> Tokens { get; } = new List<(string, int)>();
+    public Game(int id)
+    {
+        this.Id = id;
+    }
+}
---
+++ 2023/AoC_2023_02/Program.cs
@@ -0,0 +1,45 @@
+﻿var lines = File.ReadAllLines("Input.txt").ToList();
+
+List<Game> games = new List<Game>();
+for (int i = 0; i < lines.Count; i++)
+{
+    var line = lines[i];
+
+    var split = line.Split(new char[] { ':', ' ', ';', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+    var game = new Game(int.Parse(split[1]));
+    for (int x = 2; x < split.Length; x += 2)
+    {
+        var nmb = split[x];
+        var color = split[x + 1];
+
+        game.Tokens.Add((color, int.Parse(nmb)));
+    }
+    games.Add(game);
+}
+
+Console.WriteLine("Level1: " + games.Where(CheckLvl1).Sum(d => d.Id));
+Console.WriteLine("Level2: " + games.Sum(GetLvl2));
+
+decimal GetLvl2(Game game)
+{
+    return game.Tokens.GroupBy(d => d.Color).Select(c => c.Max(c => c.Count)).Aggregate((f, s) => f * s);
+}
+
+bool CheckLvl1(Game game)
+{
+    if (game.Tokens.Any(c => c.Color == "red" && c.Count > 12))
+    {
+        return false;
+    }
+    if (game.Tokens.Any(c => c.Color == "green" && c.Count > 13))
+    {
+        return false;
+    }
+    if (game.Tokens.Any(c => c.Color == "blue" && c.Count > 14))
+    {
+        return false;
+    }
+
+    return true;
+}
