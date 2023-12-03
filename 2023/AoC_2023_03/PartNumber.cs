d9fa8adf1bfceff57e3ef0f5ed2e33c8df2815ff
AEonWho <thomas.beer@outlook.com>
2023-12-03 08:00:14 +0100

    2023 - Day 03 added

---
+++ 2023/AoC_2023_03/PartNumber.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class PartNumber
+{
+    public PartNumber(int xMax, int yMax)
+    {
+        XMax = xMax;
+        YMax = yMax;
+    }
+
+    public int Value { get; set; }
+
+    public List<(int x, int y)> Coords { get; set; } = new List<(int x, int y)>();
+
+    public IEnumerable<(int x, int y)> Neighbors => GetNeighbors();
+
+    public int XMax { get; }
+    public int YMax { get; }
+
+    internal void Add(int nmb, (int charNumber, int lineNumber) value)
+    {
+        Value = Value * 10 + nmb;
+        Coords.Add(value);
+    }
+
+    private IEnumerable<(int x, int y)> GetNeighbors()
+    {
+        HashSet<(int x, int y)> neighbors = new HashSet<(int, int)>();
+
+        foreach (var coord in Coords)
+        {
+            neighbors.Add((coord.x + 1, coord.y));
+            neighbors.Add((coord.x + 1, coord.y + 1));
+            neighbors.Add((coord.x + 1, coord.y - 1));
+            neighbors.Add((coord.x, coord.y - 1));
+            neighbors.Add((coord.x, coord.y + 1));
+            neighbors.Add((coord.x - 1, coord.y));
+            neighbors.Add((coord.x - 1, coord.y + 1));
+            neighbors.Add((coord.x - 1, coord.y - 1));
+        }
+
+        return neighbors.Except(Coords).Where(c => c.x >= 0 && c.y >= 0 && c.x <= XMax && c.y <= YMax).ToList();
+    }
+}
---
+++ 2023/AoC_2023_03/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var lines = File.ReadAllLines("input.txt");
+
+
+HashSet<PartNumber> partnumbers = new HashSet<PartNumber>();
+HashSet<(int, int)> symbols = new HashSet<(int, int)>();
+HashSet<(int, int)> gears = new HashSet<(int, int)>();
+for (int lineNumber = 0; lineNumber < lines.Length; lineNumber++)
+{
+    PartNumber? currentPartNumber = null;
+    for (int charNumber = 0; charNumber < lines[lineNumber].Length; charNumber++)
+    {
+        var character = lines[lineNumber][charNumber];
+        var nmb = character - 48;
+        if (nmb >= 0 && nmb <= 9)
+        {
+            currentPartNumber ??= new PartNumber(lines[lineNumber].Length - 1, lines.Length - 1);
+            partnumbers.Add(currentPartNumber);
+            currentPartNumber.Add(nmb, (charNumber, lineNumber));
+        }
+        else
+        {
+            currentPartNumber = null;
+            if (character != '.')
+            {
+                symbols.Add((charNumber, lineNumber));
+                if (character == '*')
+                {
+                    gears.Add((charNumber, lineNumber));
+                }
+            }
+        }
+    }
+}
+
+var Level1 = partnumbers.Where(d => d.Neighbors.Any(symbols.Contains)).Sum(c => c.Value);
+Console.WriteLine("Level1: " + Level1);
+
+var level2List = gears.Select(c => partnumbers.Where(d => d.Neighbors.Contains(c)).ToList()).Where(c => c.Count() > 1).ToList();
+var level2 = level2List.Select(c => c.Aggregate<PartNumber, int>(1, (first, second) => first * second.Value));
+Console.WriteLine("Level2: " + level2.Sum());
