0273d7e765b736773bfaa9ac83fb937c40086af0
AEonWho <thomas.beer@outlook.com>
2023-12-13 10:07:43 +0100

    2023 - Day 13 added

---
+++ 2023/AoC_2023_13/Map.cs
@@ -0,0 +1,34 @@
+﻿
+using AoC_Common.PathFinding;
+
+internal class Map
+{
+    public long X_Max { get; set; }
+
+    public long Y_Max { get; set; }
+
+    public Dictionary<(long X, long Y), char> Data { get; set; } = new Dictionary<(long, long), char>();
+
+    internal void Add((int x, int y) point, char v)
+    {
+        if (point.x + 1 > X_Max)
+            X_Max = point.x + 1;
+        if (point.y + 1 > Y_Max)
+            Y_Max = point.y + 1;
+
+        Data[point] = v;
+    }
+
+    public void Print()
+    {
+        for (int y = 0; y < Y_Max; y++)
+        {
+            for (int x = 0; x < X_Max; x++)
+            {
+                Console.Write(Data[(x, y)]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 2023/AoC_2023_13/Program.cs
@@ -0,0 +1,112 @@
+﻿var input = File.ReadAllLines("Input.txt");
+
+List<Map> _maps = [new Map()];
+{
+    var y = 0;
+    foreach (var line in input)
+    {
+        if (string.IsNullOrWhiteSpace(line))
+        {
+            y = 0;
+            _maps.Add(new Map());
+            continue;
+        }
+        for (var x = 0; x < line.Length; x++)
+        {
+            _maps[^1].Add((x, y), line[x]);
+        }
+        y++;
+    }
+}
+
+int sumLevel1 = 0;
+int sumLevel2 = 0;
+foreach (var map in _maps)
+{
+    int XSplitLevel1 = -1;
+    int XSplitLevel2 = -1;
+    for (int i = 0; i < map.X_Max - 1; i++)
+    {
+        int errorCount = 0;
+        var iFirst = i;
+        var iSecond = i + 1;
+        while (iFirst >= 0 && iSecond < map.X_Max)
+        {
+            for (int y = 0; y < map.Y_Max; y++)
+            {
+                if (map.Data[(iFirst, y)] != map.Data[(iSecond, y)])
+                {
+                    errorCount++;
+                }
+            }
+
+            iFirst--;
+            iSecond++;
+        }
+
+        if (errorCount == 0)
+        {
+            XSplitLevel1 = i;
+        }
+
+        if (errorCount == 1)
+        {
+            XSplitLevel2 = i;
+        }
+    }
+
+    if (XSplitLevel1 != -1)
+    {
+        sumLevel1 += (XSplitLevel1 + 1);
+    }
+
+    if (XSplitLevel2 != -1)
+    {
+        sumLevel2 += (XSplitLevel2 + 1);
+    }
+
+    int YSplitLevel1 = -1;
+    int YSplitLevel2 = -1;
+    for (int i = 0; i < map.Y_Max - 1; i++)
+    {
+        int errorCount = 0;
+        var iFirst = i;
+        var iSecond = i + 1;
+        while (iFirst >= 0 && iSecond < map.Y_Max)
+        {
+            for (int x = 0; x < map.X_Max; x++)
+            {
+                if (map.Data[(x, iFirst)] != map.Data[(x, iSecond)])
+                {
+                    errorCount++;
+                }
+            }
+
+            iFirst--;
+            iSecond++;
+        }
+
+        if (errorCount == 0)
+        {
+            YSplitLevel1 = i;
+        }
+
+        if (errorCount == 1)
+        {
+            YSplitLevel2 = i;
+        }
+    }
+
+    if (YSplitLevel1 != -1)
+    {
+        sumLevel1 += 100 * (YSplitLevel1 + 1);
+    }
+
+    if (YSplitLevel2 != -1)
+    {
+        sumLevel2 += 100 * (YSplitLevel2 + 1);
+    }
+}
+
+Console.WriteLine(sumLevel1);
+Console.WriteLine(sumLevel2);
