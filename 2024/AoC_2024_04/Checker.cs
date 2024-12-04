2fd9f3ac0b4acdd732ae1e57f3cfb6be40e42e65
AEonWho <thomas.beer@outlook.com>
2024-12-04 14:50:39 +0100

    2024 - Day 04

---
+++ 2024/AoC_2024_04/Checker.cs
@@ -0,0 +1,41 @@
+﻿using AoC_Common.PathFinding;
+
+public class Checker
+{
+    public Checker(MapCoordinate start, MapDirection dir, string check = "XMAS")
+    {
+        Start = start;
+        Direction = dir;
+        Check = check;
+    }
+
+    public MapCoordinate Start { get; }
+
+    public MapDirection Direction { get; }
+
+    public string Check { get; }
+
+    public IEnumerable<MapCoordinate> GetCoordinates()
+    {
+        yield return Start; 
+        var next = Start.MoveDirection(Direction);
+        yield return next;
+        for (int i = 0; i < Check.Length - 2; i++)
+        {
+            next = next.MoveDirection(Direction);
+            yield return next;
+        }
+    }
+
+    public bool IsValid(Dictionary<MapCoordinate, char> map)
+    {
+        var coords = GetCoordinates().ToArray();
+
+        if (coords.All(map.ContainsKey))
+        {
+            return new string(coords.Select(c => map[c]).ToArray()) == Check;
+        }
+
+        return false;
+    }
+}
---
+++ 2024/AoC_2024_04/Program.cs
@@ -0,0 +1,53 @@
+﻿using AoC_Common.PathFinding;
+
+var lines = File.ReadAllLines("Input.txt");
+
+var map = new Dictionary<MapCoordinate, char>();
+
+var sizeY = lines.Length;
+var sizeX = lines[0].Length;
+
+for (int y = 0; y < sizeY; y++)
+{
+    for (int x = 0; x < sizeX; x++)
+    {
+        map[(x, y)] = lines[y][x];
+    }
+}
+
+var success = new List<Checker>();
+foreach (var coord in map.Keys)
+{
+    var checkers = new List<Checker>()
+    {
+        new Checker(coord, MapDirection.N),
+        new Checker(coord, MapDirection.NE),
+        new Checker(coord, MapDirection.E),
+        new Checker(coord, MapDirection.SE),
+        new Checker(coord, MapDirection.S),
+        new Checker(coord, MapDirection.SW),
+        new Checker(coord, MapDirection.W),
+        new Checker(coord, MapDirection.NW)
+    };
+
+    success.AddRange(checkers.Where(c => c.IsValid(map)));
+}
+
+Console.WriteLine("Level1: " + success.Count());
+
+success = new List<Checker>();
+foreach (var coord in map.Keys)
+{
+    var checkers = new List<Checker>()
+    {
+        new Checker(coord, MapDirection.NE, "MAS"),
+        new Checker(coord, MapDirection.SE, "MAS"),
+        new Checker(coord, MapDirection.SW, "MAS"),
+        new Checker(coord, MapDirection.NW, "MAS")
+    };
+
+    success.AddRange(checkers.Where(c => c.IsValid(map)));
+}
+
+var test = success.GroupBy(d => d.GetCoordinates().ToArray()[1]).Where(d => d.Count() > 1).ToList();
+Console.WriteLine("Level2: " + test.Count());
