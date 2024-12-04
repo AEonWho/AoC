e962a57a855b9119bf30f19a035b71a86c736205
AEonWho <thomas.beer@outlook.com>
2024-12-04 14:50:33 +0100

    2024 - Day 03

---
+++ 2024/AoC_2024_03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+var text = File.ReadAllText("Input.txt");
+
+var regex = Regex.Matches(text, "mul\\(([1-9][0-9]*),([1-9][0-9]*)\\)");
+
+var result = 0;
+foreach (Match match in regex)
+{
+    var first = match.Groups[1].Value;
+    var second = match.Groups[2].Value;
+
+    result += int.Parse(first) * int.Parse(second);
+}
+
+Console.WriteLine("Level1: " + result);
+
+result = 0;
+foreach (Match match in regex)
+{
+    var textBefore = text.Substring(0, match.Index);
+
+    if (textBefore.LastIndexOf("don't()") > textBefore.LastIndexOf("do()"))
+        continue;
+
+    var first = match.Groups[1].Value;
+    var second = match.Groups[2].Value;
+
+    result += int.Parse(first) * int.Parse(second);
+}
+
+Console.WriteLine("Level2: " + result);
