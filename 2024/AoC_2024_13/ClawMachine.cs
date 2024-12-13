cc9dbc4ba25af3c8732b4784a995c8a05ffb90a8
AEonWho <thomas.beer@outlook.com>
2024-12-13 09:22:53 +0100

    2024 - Day 13

---
+++ 2024/AoC_2024_13/ClawMachine.cs
@@ -0,0 +1,67 @@
+﻿
+internal class ClawMachine
+{
+
+    public ClawMachine(string resultX, string resultY, string buttonAX, string buttonAY, string buttonBX, string buttonBY)
+    {
+        ResultX = long.Parse(resultX);
+        ResultY = long.Parse(resultY);
+        ButtonAX = long.Parse(buttonAX);
+        ButtonAY = long.Parse(buttonAY);
+        ButtonBX = long.Parse(buttonBX);
+        ButtonBY = long.Parse(buttonBY);
+    }
+
+    public void InitLvl2()
+    {
+        ResultX += 10000000000000;
+        ResultY += 10000000000000;
+    }
+
+    public long ResultX { get; private set; }
+    public long ResultY { get; private set; }
+    public long ButtonAX { get; }
+    public long ButtonAY { get; }
+    public long ButtonBX { get; }
+    public long ButtonBY { get; }
+
+    internal IEnumerable<(long, long)> SimulateGameLevel1()
+    {
+        var maxBCountX = ResultX / ButtonBX;
+        var maxBCountY = ResultY / ButtonBY;
+
+        var bMax = Math.Min(maxBCountX, maxBCountY);
+
+        for (var bCount = bMax; bCount > 0; bCount--)
+        {
+            var offsetX = ResultX - (bCount * ButtonBX);
+            var offsetY = ResultY - (bCount * ButtonBY);
+            var res1 = offsetX % ButtonAX;
+            if (res1 != 0)
+                continue;
+
+            var res2 = offsetY % ButtonAY;
+
+            if (res1 == res2)
+            {
+                var acnt1 = offsetX / ButtonAX;
+                var acnt2 = offsetY / ButtonAY;
+
+                if (acnt1 == acnt2)
+                {
+                    yield return (acnt2, bCount);
+                }
+            }
+        }
+    }
+
+    internal IEnumerable<(long, long)> SimulateGameLevel2()
+    {
+        //Whatever Logic ausn Internet...
+        var b = (ResultY * ButtonAX - ResultX * ButtonAY) / ((ButtonAY * -1L) * ButtonBX + ButtonBY * ButtonAX);
+        var a = (ResultX - ButtonBX * b) / ButtonAX;
+
+        if (a * ButtonAX + b * ButtonBX == ResultX && a * ButtonAY + b * ButtonBY == ResultY)
+            yield return (a, b);
+    }
+}
---
+++ 2024/AoC_2024_13/Program.cs
@@ -0,0 +1,42 @@
+﻿var lines = await File.ReadAllLinesAsync("Input.txt");
+
+List<ClawMachine> machines = new List<ClawMachine>();
+for (int i = 0; i < lines.Length; i += 4)
+{
+    var split = lines[i].Split(['+', ',', ' '], StringSplitOptions.RemoveEmptyEntries);
+    var split2 = lines[i + 1].Split(['+', ',', ' '], StringSplitOptions.RemoveEmptyEntries);
+    var split3 = lines[i + 2].Split(['=', ',', ' '], StringSplitOptions.RemoveEmptyEntries);
+
+
+    machines.Add(new ClawMachine(split3[^3], split3[^1], split[^3], split[^1], split2[^3], split2[^1]));
+}
+
+long sum = 0;
+foreach (var m in machines)
+{
+    var score = m.SimulateGameLevel1().ToList();
+
+    if (score.Any())
+    {
+        sum += score.Select(d => d.Item1 * 3 + d.Item2 * 1).OrderBy(d => d).FirstOrDefault();
+    }
+}
+
+Console.WriteLine("Level 1: " + sum);
+
+
+
+sum = 0;
+foreach (var m in machines)
+{
+    m.InitLvl2();
+
+    var score = m.SimulateGameLevel2().ToList();
+
+    if (score.Any())
+    {
+        sum += score.Select(d => d.Item1 * 3 + d.Item2 * 1).OrderBy(d => d).FirstOrDefault();
+    }
+}
+
+Console.WriteLine("Level 2: " + sum);
